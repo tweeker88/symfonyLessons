e41028ad1468f6f28fa76ef6ff89882be2db6c3a
tweeker88 <maxim.teplov97@gmail.com>
2018-12-24 15:36:58 +0300

    feat(1.6): Добавил новый шаблон

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class ArticleController
  * @package App\Controller
  */
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -24,7 +25,16 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('This page with slug %s', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig',[
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 
 
