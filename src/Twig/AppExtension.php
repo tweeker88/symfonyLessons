19b80f4aa2effb5b58e7a30ba4967a566248937e
tweeker88 <mot@linkprofit.com>
2019-01-06 14:07:28 +0300

    feat(3.6): Создал свой twig фильтр

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    private $helper;
+
+    public function __construct(MarkdownHelper $helper)
+    {
+
+        $this->helper = $helper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+    public function processMarkdown($value)
+    {
+        return $this->helper->parse($value);
+    }
+
+}
