ecb62eac0066ca8bbeb19b293b01bd795cc1e189
tweeker88 <mot@linkprofit.com>
2019-02-10 13:03:56 +0300

    feat(fixtures): Добавил зависимости фикстур

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,22 +4,36 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixtures
+class CommentFixture extends BaseFixtures implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Comment::class, 100, function (Comment $comment){
+        $this->createMany(Comment::class, 100, function (Comment $comment) {
             $comment->setContent(
                 $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentence(2, true)
             );
 
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
-            $comment->setArticle($this->getReference(Article::class.'_0'));
+            $comment->setArticle($this->getRandomReference(Article::class));
         });
 
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies(): array
+    {
+        return [
+            ArticleFixtures::class
+        ];
     }
 }
