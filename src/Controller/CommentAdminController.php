50c11a13cb9be02a0bc8a6bf9b0444e6f4dfdefa
tweeker88 <mot@linkprofit.com>
2019-02-11 10:51:43 +0300

    feat(4.12): добавил twig extension

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,11 +10,15 @@ class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @param CommentRepository $repository
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(CommentRepository $repository)
     {
+        $comments = $repository->findBy([],['createdAt' => 'DESC']);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments
         ]);
     }
 }
