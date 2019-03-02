8ad9d1c15cd2bf45be2f058f0cc476cf6a134275
tweeker88 <mot@linkprofit.com>
2019-03-02 19:45:43 +0300

    feat(5.5): добавил аутентификатор

---
+++ src/Security/LoginFormAuthenticatorAuthenticator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticatorAuthenticator extends AbstractFormLoginAuthenticator
+{
+    public function supports(Request $request)
+    {
+
+    }
+
+    public function getCredentials(Request $request)
+    {
+        // todo
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // todo
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // todo
+    }
+
+    /**
+     * Return the URL to the login page.
+     *
+     * @return string
+     */
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+}
