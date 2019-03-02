8ba7f05785a4d6c73b8c5c25d190fe11bd7d3d7a
tweeker88 <mot@linkprofit.com>
2019-03-02 20:03:57 +0300

    feat(5.6): добавил реализацию LoginFormAuth

--- src/Security/LoginFormAuthenticatorAuthenticator.php
+++ src/Security/LoginFormAuthenticatorAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -10,29 +11,47 @@ use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticato
 
 class LoginFormAuthenticatorAuthenticator extends AbstractFormLoginAuthenticator
 {
-    public function supports(Request $request)
+
+    /** @var UserRepository $userRepository*/
+    private $userRepository;
+
+    /**
+     * LoginFormAuthenticatorAuthenticator constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
     {
 
+        $this->userRepository = $userRepository;
+    }
+
+    public function supports(Request $request): bool
+    {
+        return $request->attributes->get('_route') === 'app_login'
+            && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
     {
-        // todo
+        return [
+            'email' => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+        ];
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        return true;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // todo
+        dd('Done');
     }
 
     /**
