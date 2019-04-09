2eafbd8a7e3fd699d027f14c65dfb04862efd8c8
tweeker88 <maxim.teplov97@gmil.com>
2019-04-09 11:58:37 +0300

    feat(5.25): Добавил ApiToken

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -22,7 +23,7 @@ class UserFixtures extends BaseFixtures
 
     public function loadData(ObjectManager $manager)
     {
-        $this->createManyInt(10, 'main_users', function ($i) {
+        $this->createManyInt(10, 'main_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
@@ -32,6 +33,14 @@ class UserFixtures extends BaseFixtures
                 $user,
                 'password'
             ));
+
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+
+
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
+
             return $user;
         });
 
